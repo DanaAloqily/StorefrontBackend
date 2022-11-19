3fc3ac894c85eed12927a8091c0cf41dc517a3ff
DanaAloqily <danaaloqily@hotmail.com>
2022-11-19 18:16:26 +0300

    Database migration setup

---
+++ migrations/sqls/20221119150614-users-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE users(ID SERIAL PRIMARY KEY,
+fisrtName VARCHAR(50) NOT NULL,
+lastName VARCHAR(50) NOT NULL,
+password VARCHAR(255) NOT NULL
+);
