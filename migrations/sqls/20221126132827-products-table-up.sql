142671650ce0dc9fedbacf6f109bcd1ab2697b57
DanaAloqily <danaaloqily@hotmail.com>
2022-11-30 15:08:27 +0300

    Update 20221126132827-products-table-up.sql

--- migrations/sqls/20221126132827-products-table-up.sql
+++ migrations/sqls/20221126132827-products-table-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE products(id SERIAL PRIMARY KEY,
 productName VARCHAR(50) NOT NULL,
 productPrice VARCHAR(50) NOT NULL,
-productCategory VARCHAR(50) NOT NULL
+productCategory VARCHAR(50) 
 );
