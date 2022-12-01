49895b0ae5b35c17476408743a7dd4048dcca09c
DanaAloqily <danaaloqily@hotmail.com>
2022-12-01 08:49:47 +0300

    comments

--- src/routes/api/products.routes.ts
+++ src/routes/api/products.routes.ts
@@ -1,22 +1,19 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import express, { Request, Response } from 'express';
 import * as controllers from '../../controllers/product.controllers';
 
 const products_routes = Router();
 
-products_routes.get('/',(req: Request, res:Response) =>{
+/* products_routes.get('/',(req: Request, res:Response) =>{
     res.status(200).json({
         message:'hello world from products🛍'
     })
-} ) 
-products_routes.route('/')
-.get(controllers.index).post(controllers.create)
-products_routes.route('/:id')
-.get(controllers.show)
+} )  */
+products_routes.route('/').get(controllers.index).post(controllers.create);
+products_routes.route('/:id').get(controllers.show);
 
 //routes.get(./ , );
 //routes.post(./ , );
 //...
-
 
 export default products_routes;
